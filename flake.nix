fd5ed3432626e238bcf69b300997a41fc67d8015
Joel Granados <j.granados@samsung.com>
2025-02-21 10:53:31 +0100

    Bring in the linux kernel headers
    Having the Linux headers available is necessary to activate the IOMMUFD
    path. It depends on HAVE_IOMMU_FAULT_QUEUE_ALLOC being defined in the
    iommufd.h uAPI; available since 6.11.
    
    Since the nixos package that provided the linux headers only went to
    6.10 in unstable, we add a temporary reference to a nixos repository
    that provides the headers from 6.11.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "provides the libvfn package for NixOS and Nix environments";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
+    unstable.url = "github:Joelgranados/nixpkgs/master";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, unstable }:
     let
       libvfnVersion = "5.1.0";
       # lists supported systems
@@ -13,19 +13,23 @@
 
       forAllSystems = fn:
         nixpkgs.lib.genAttrs allSystems
-        (system: fn { pkgs = import nixpkgs { inherit system; }; });
-
+        (system: fn {
+          pkgs = import nixpkgs { inherit system; };
+          uPkgs = import unstable { inherit system; };
+          });
     in {
       formatter = forAllSystems ({ pkgs }: pkgs.nixfmt);
-      packages = forAllSystems ({ pkgs }: rec {
-        libvfn = pkgs.stdenv.mkDerivation {
+      packages = forAllSystems ({ pkgs, uPkgs }: rec {
+        libvfn = pkgs.stdenv.mkDerivation rec {
           pname = "libvfn";
           version = libvfnVersion;
           src = ./.;
+          kernelHeaders = uPkgs.linuxHeaders;
           mesonFlags = [
             "-Ddocs=disabled"
             "-Dlibnvme=disabled"
             "-Dprofiling=false"
+            "-Dlinux-headers=${kernelHeaders}/include/"
           ];
           nativeBuildInputs = with pkgs; [ meson ninja pkg-config perl ];
         };
