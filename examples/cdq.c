4638576e51da53f7de0f2437910713b2e05c68d9
Joel Granados <j.granados@samsung.com>
2025-04-25 15:41:28 +0200

    cdq: Separate example command into actions
    This moves away from the one exec example, but is usefull to make run
    the executable within a script.
    
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- examples/cdq.c
+++ examples/cdq.c
@@ -24,9 +24,12 @@
 #include "linux/nvme_ioctl.h"
 
 static char *parent_cntl = "";
+static char *action = "";
 static unsigned int cntlid = 0;
+static unsigned int cdqid = UINT_MAX;
 static unsigned int entry_nbyte = 0;
 static unsigned int entry_nr = 0;
+static unsigned int verbose = 0;
 bool s_usage;
 
 static struct opt_table opts[] = {
@@ -34,39 +37,39 @@ static struct opt_table opts[] = {
 	OPT_WITH_ARG("-C|--child-cntl",
 			opt_set_uintval, opt_show_uintval,
 			&cntlid, "Child controller ID"),
+	OPT_WITH_ARG("--cdq-id",
+			opt_set_uintval, opt_show_uintval,
+			&cdqid, "Controller Data Queue Identifier"),
 	OPT_WITH_ARG("-P|--parent-cntl",
 			opt_set_charp, opt_show_charp,
 			&parent_cntl, "Parent controller device path"),
+	OPT_WITH_ARG("-A|--action",
+			opt_set_charp, opt_show_charp,
+			&action, "Action to perform: alloc, start, read"),
 	OPT_WITH_ARG("--entry-nbyte",
 			opt_set_uintval, opt_show_uintval,
 			&entry_nbyte, "Size in bytes of the CDQ entries"),
 	OPT_WITH_ARG("--entry-nr",
 			opt_set_uintval, opt_show_uintval,
 			&entry_nr, "Number of entries in CDQ"),
+	OPT_WITH_ARG("--verbose",
+			opt_set_uintval, opt_show_uintval,
+			&verbose, "Verbosity value"),
 	OPT_ENDTABLE,
 };
 
-int main(int argc, char **argv)
+int do_action_alloc(void)
 {
 	int fd;
 	struct nvme_cdq_cmd cdq_cmd;
 
-	opt_register_table(opts, NULL);
-	opt_parse(&argc, argv, opt_log_stderr_exit);
-
-	if (s_usage)
-		opt_usage_and_exit(NULL);
-
-	if (streq(parent_cntl, ""))
-		opt_usage_exit_fail("missing --parent-cntl (parent controller device path)");
-
 	if (entry_nbyte == 0 || entry_nr == 0)
 		opt_usage_exit_fail("--entry-nbyte and --entry-nr need to be >0");
 
-	opt_free_table();
-
-	fprintf(stderr, "child cntl : %d\n", cntlid);
-	fprintf(stderr, "parent path : %s\n", parent_cntl);
+	if (verbose > 0) {
+		fprintf(stderr, "child cntl : %d\n", cntlid);
+		fprintf(stderr, "parent path : %s\n", parent_cntl);
+	}
 
 	fd = open(parent_cntl, O_RDWR);
 	if (fd < 0) {
@@ -82,16 +85,68 @@ int main(int argc, char **argv)
 		log_debug("failed on NVME_CDQ_ADM_FLAGS_ALLOC");
 		return -1;
 	}
-	uint16_t cdqid = cdq_cmd.alloc.cdqid;
 
-	memset(&cdq_cmd, 0, sizeof(cdq_cmd));
+	fprintf(stdout, "%d\n", cdq_cmd.alloc.cdqid);
+	return 0;
+}
+
+int do_action_start(void)
+{
+
+	int fd;
+	struct nvme_cdq_cmd cdq_cmd = {};
+	uint16_t u16_cdqid = 0;
+
+	if (cdqid == UINT_MAX)
+		opt_usage_exit_fail("missing controller data queue id");
+	u16_cdqid = (uint16_t)cdqid;
+
+	fd = open(parent_cntl, O_RDWR);
+	if (fd < 0) {
+		log_debug("failed to open parent controller device path: %s\n", strerror(errno));
+		return -1;
+	}
+
 	cdq_cmd.flags = NVME_CDQ_ADM_FLAGS_TR_SEND;
 	cdq_cmd.tr_send.action = NVME_CDQ_ADM_FLAGS_TR_SEND_START;
-	cdq_cmd.tr_send.cdqid = cdqid;
+	cdq_cmd.tr_send.cdqid = u16_cdqid;
 	if (ioctl(fd, NVME_IOCTL_ADMIN_CDQ, &cdq_cmd)) {
 		log_debug("failed on NVME_CDQ_ADM_FLAGS_TR_SEND");
 		return -1;
 	}
+
+	return 0;
+}
+
+int do_action_read(void)
+{
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	opt_register_table(opts, NULL);
+	opt_parse(&argc, argv, opt_log_stderr_exit);
+
+	if (s_usage)
+		opt_usage_and_exit(NULL);
+
+	if (streq(parent_cntl, ""))
+		opt_usage_exit_fail("missing --parent-cntl (parent controller device path)");
+
+	if (streq(action, ""))
+		opt_usage_exit_fail("missing --action");
+
+	opt_free_table();
+
+	if(streq(action, "alloc")) {
+		return do_action_alloc();
+	} else if (streq(action, "start")) {
+		return do_action_start();
+	} else if (streq(action, "read")) {
+		return do_action_read();
+	} else
+		opt_usage_exit_fail("incorrect action string %s", action);
 
 	return 0;
 }
