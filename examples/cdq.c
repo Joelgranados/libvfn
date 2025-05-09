0376d92261f97137543a6addd2b4a74bbb8539d2
Joel Granados <j.granados@samsung.com>
2025-05-09 09:36:53 +0200

    cdq: Add call to create and read the cdq file descriptor
    Signed-off-by: Joel Granados <j.granados@samsung.com>

--- examples/cdq.c
+++ examples/cdq.c
@@ -45,7 +45,7 @@ static struct opt_table opts[] = {
 			&parent_cntl, "Parent controller device path"),
 	OPT_WITH_ARG("-A|--action",
 			opt_set_charp, opt_show_charp,
-			&action, "Action to perform: alloc, tr_send_start, kthread"),
+			&action, "Action to perform: alloc, tr_send_start, kthread, readfd"),
 	OPT_WITH_ARG("--entry-nbyte",
 			opt_set_uintval, opt_show_uintval,
 			&entry_nbyte, "Size in bytes of the CDQ entries"),
@@ -140,6 +140,93 @@ int do_action_kthread(void)
 	return 0;
 }
 
+void hexdump(const void *data, size_t size) {
+    const unsigned char *byte = (const unsigned char *)data;
+    size_t i, j;
+
+    for (i = 0; i < size; i += 16) {
+        printf("%08zx  ", i);  // Offset
+
+        // Hex bytes
+        for (j = 0; j < 16; ++j) {
+            if (i + j < size)
+                printf("%02x ", byte[i + j]);
+            else
+                printf("   ");
+        }
+
+        printf(" ");
+
+        // ASCII chars
+        for (j = 0; j < 16; ++j) {
+            if (i + j < size) {
+                unsigned char c = byte[i + j];
+                printf("%c", isprint(c) ? c : '.');
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int do_action_readfd(void)
+{
+	int ret, fd, cdq_fd;
+	void *buf;
+	uint max_retries = 50;
+	size_t buf_size;
+	struct nvme_cdq_cmd cdq_cmd = {};
+
+	if (cdqid == UINT_MAX)
+		opt_usage_exit_fail("missing controller data queue id");
+
+	if (entry_nbyte == 0 || entry_nr == 0)
+		opt_usage_exit_fail("--entry-nbyte and --entry-nr need to be >0");
+
+	fd = open(parent_cntl, O_RDWR);
+	if (fd < 0) {
+		log_debug("failed to open parent controller device path: %s\n", strerror(errno));
+		return -1;
+	}
+
+	cdq_cmd.flags = NVME_CDQ_ADM_FLAGS_READFD;
+	cdq_cmd.readfd.cdqid = (uint16_t)cdqid;
+	if (ioctl(fd, NVME_IOCTL_ADMIN_CDQ, &cdq_cmd)) {
+		log_debug("failed on NVME_IOCTL_ADM_FLASG_READFD");
+		return -1;
+	}
+
+	cdq_fd = cdq_cmd.readfd.read_fd;
+	log_debug("File descriptor CDQ: (%d)\n", cdq_fd);
+
+	buf_size = entry_nbyte * entry_nr;
+	buf = malloc(buf_size);
+	hexdump(buf, buf_size);
+	if (!buf){
+		log_debug("failed in malloc");
+		close(cdq_fd);
+	}
+
+	for (max_retries = UINT_MAX - max_retries; max_retries != 0; ++max_retries)
+	{
+		sleep(1);
+		log_debug("Reading on %d for %ld\n", cdq_fd, buf_size);
+		ret = read(cdq_fd, buf, buf_size);
+		if (ret < 0) {
+			log_debug("failed on entries read");
+			return -1;
+		}
+		if (ret == 0) {
+			log_debug("read returned 0... try again, try number %d\n", UINT_MAX - max_retries);
+			continue;
+		} else {
+			hexdump(buf, buf_size);
+		}
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	opt_register_table(opts, NULL);
@@ -162,6 +249,8 @@ int main(int argc, char **argv)
 		return do_action_trsend(NVME_CDQ_ADM_FLAGS_TR_SEND_START);
 	} else if (streq(action, "kthread")) {
 		return do_action_kthread();
+	} else if (streq(action, "readfd")) {
+		return do_action_readfd();
 	} else
 		opt_usage_exit_fail("incorrect action string %s", action);
 
